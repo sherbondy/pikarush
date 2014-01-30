e2d8f0f64e84415bc19658ae84c430df971a271d
Ethan Sherbondy <sherbondye@gmail.com>
2014-01-29 21:22:13 -0500

    spitting calendar might have owkred.

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject pikarush "0.0.1"
   :description "pika rush website 2013"
-  :dependencies [[prismatic/dommy "0.1.1"]
-                 [cljs-ajax "0.2.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-ical "1.1"]
+                 [prismatic/dommy "0.1.1"]
+                 [cljs-ajax "0.2.0"]
+                 [clj-time "0.6.0"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
 
   :cljsbuild
--- src/pikarush/calendar.clj
+++ src/pikarush/calendar.clj
@@ -1,29 +1,39 @@
 (ns pikarush.calendar
   (:require [clj-ical.format :as ical]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import org.joda.time.DateTime))
 
 ;; export a ical file from the rush events list
 
-;; (defn map-to-int [v]
-;;   (map (fn [n] (Integer. n))
-;;        v))
-
-;; (ical/write-object
-;;  [:vcalendar
-;;   (for [event events]
-;;     [:vevent
-;;      [:summary (:name event)]
-;;      [:description (:description event)]
-;;      ;; ha, hard coding
-;;      (let [year 2014
-;;            [month day] (map-to-int (str/split (:date event) "/"))
-;;            [start-hour start-minute] (map-to-int (str/split (nth time 0) ":"))]
-;;        [:dtstart (org.joda.time.DateTime. (Integer. ))]
-;;        [:dtend   (org.joda.time.DateTime. )])])])
+(defn map-to-int [v]
+  (map (fn [n] (Integer. n))
+       v))
 
 (def events (read-string (slurp "events/spring2014.edn")))
 
-(defn spit-wiki-text [events]
+(defn ical-event [events]
+  (ical/write-object
+   [:vcalendar
+     (for [event events]
+       (let [year 2014
+             [month day]               (map-to-int (str/split (:date event) #"/"))
+              month (inc month)
+             [start-hour start-minute] (map-to-int (str/split (nth (:time event) 0) #":"))
+             [end-hour end-minute]     (map-to-int (str/split (nth (:time event) 1) #":"))]
+         [:vevent
+           [:summary (:name event)]
+           [:description (:description event)]
+           ;; ha, hard coding
+           [:dtstart (org.joda.time.DateTime. 2014 month day start-hour start-minute)]
+           [:dtend   (org.joda.time.DateTime. 2014 month day end-hour end-minute)]]))]))
+
+(def calendar
+  (with-out-str
+    (ical-event events)))
+
+(spit "pika-events.ics" calendar)
+
+(defn wiki-text [events]
   (let [events-by-date (group-by :date events)
         dates (sort (keys events-by-date))]
     (apply str
@@ -36,4 +46,4 @@
                    "'''Location:''' " (:location event) "\n\n"
                    (:description event) "\n\n"))))))))
 
-(spit "events.mw" (spit-wiki-text events))
+;; (spit "events.mw" (wiki-text events))
