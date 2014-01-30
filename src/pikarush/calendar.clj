368758c11bf68df0193f587dc26b8fd68c001b89
Ethan Sherbondy <sherbondye@gmail.com>
2014-01-29 21:01:35 -0500

    spit out mediawiki events.

--- src/pikarush/calendar.clj
+++ src/pikarush/calendar.clj
@@ -1,23 +1,39 @@
 (ns pikarush.calendar
   (:require [clj-ical.format :as ical]
-            [pikarush.events :refer events]
             [clojure.string :as str]))
 
 ;; export a ical file from the rush events list
 
-(defn map-to-int [v]
-  (map (fn [n] (Integer. n))
-       v))
+;; (defn map-to-int [v]
+;;   (map (fn [n] (Integer. n))
+;;        v))
 
-(ical/write-object
- [:vcalendar
-  (for [event events]
-    [:vevent
-     [:summary (:name event)]
-     [:description (:description event)]
-     ;; ha, hard coding
-     (let [year 2014
-           [month day] (map-to-int (str/split (:date event) "/"))
-           [start-hour start-minute] (map-to-int (str/split (nth time 0) ":"))]
-       [:dtstart (org.joda.time.DateTime. (Integer. ))]
-       [:dtend   (org.joda.time.DateTime. )])])])
+;; (ical/write-object
+;;  [:vcalendar
+;;   (for [event events]
+;;     [:vevent
+;;      [:summary (:name event)]
+;;      [:description (:description event)]
+;;      ;; ha, hard coding
+;;      (let [year 2014
+;;            [month day] (map-to-int (str/split (:date event) "/"))
+;;            [start-hour start-minute] (map-to-int (str/split (nth time 0) ":"))]
+;;        [:dtstart (org.joda.time.DateTime. (Integer. ))]
+;;        [:dtend   (org.joda.time.DateTime. )])])])
+
+(def events (read-string (slurp "events/spring2014.edn")))
+
+(defn spit-wiki-text [events]
+  (let [events-by-date (group-by :date events)
+        dates (sort (keys events-by-date))]
+    (apply str
+      (for [date dates]
+        (str "=== " date " ===\n")
+        (apply str
+          (for [event (get events-by-date date)]
+            (str "==== " (:name event) " ====\n"
+                 "'''Time:''' " (first (:time event)) "-" (second (:time event)) "\n"
+                 "'''Location:''' " (:location event) "\n"
+                 (:description event) "\n")))))))
+
+(spit-wiki-text events)
